0e4e8bfada83b49da4baa89c3f9613f3baa820aa
hintonlever <b.hintonlever@gmail.com>
2019-01-14 12:53:25 +1000

    Add files via upload

--- R/dim_helpers.R
+++ R/dim_helpers.R
@@ -1,1 +1,13 @@
-test
+rsplit <- function(dd) {
+  col <- names(dd)[1]
+  print(col)
+  dat <- unique(dd[[1]])
+  xx <- lapply(unique(dat), function(x) {
+    z <- setNames(list(x), col)
+    if(ncol(dd)>1) {
+      z[[names(dd)[2]]] <- rsplit(dd[dat==x,-1, drop=FALSE])
+    }
+    z
+  })
+  return(xx)
+}
